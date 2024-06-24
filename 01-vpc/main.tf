e64657fda379709718b38fa210ebc66f686a2a31
gurava1234 <bachuguravareddy47@gmail.com>
2024-06-24 10:13:23 +0530

    first commit

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,4 +1,4 @@
-module "roboshop" {
+module "robosho" {
   #source = "../terraform-aws-vpc"
   source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
